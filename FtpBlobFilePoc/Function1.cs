bd31377ef340c77ca0c84a83091f877edebecc40
Ryan Smothers <rsmothers@karmak.com>
2020-04-07 15:04:18 -0500

    check point check in on poc.  files are picked up and sent via sftp.  next need to remove from blob storage after successful send.

--- FtpBlobFilePoc/Function1.cs
+++ FtpBlobFilePoc/Function1.cs
@@ -1,10 +1,14 @@
-using Microsoft.Azure.WebJobs;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using Chilkat;
 
 namespace FtpBlobFilePoc
 {
@@ -18,62 +22,87 @@ namespace FtpBlobFilePoc
                     "rsmothersstorage",
                     "Z5BlDnPCAvfQEXu9Bi/4Zeu1hqk//CFgdzagOJlghGCLABtCwCIOmlklJBFYdt9kMoCIlCm2XyJw+asNKG1Mqg=="));
 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"Check for files to ftp: {DateTime.Now}");
 
-            // can i see a blob file?
-            //var result = BlobExistsOnCloud(
-            //    client,
-            //    "ftp-blob-file-poc",
-            //    "testFolder/testfile.xml");
-
-            //if (result.Result)
-            //{
-            //    log.LogInformation($"Found blob testfile.xml");
-            //}
-            //else
-            //{
-            //    log.LogInformation("blob not found.");
-            //}
-
-            // this is how i will look for files
-            var filesPresent = FilesToFtp(
-                client,
-                "ftp-blob-file-poc",
-                "testFolder",
+            FtpBlobStreams(
+                RetrieveBlobList(
+                    client,
+                    "ftp-blob-file-poc",
+                    "testFolder",
+                    log).Result,
                 log);
-
-            log.LogInformation(
-                filesPresent.Result.Results.Any() 
-                    ? "Files present to ftp." 
-                    : "No files detected.");
         }
 
-        //public static async Task<bool> BlobExistsOnCloud(CloudBlobClient client,
-        //    string containerName, string key)
-        //{
-        //    return await client.GetContainerReference(containerName)
-        //        .GetBlockBlobReference(key)
-        //        .ExistsAsync();
-        //}
-
-        public static async Task<BlobResultSegment> FilesToFtp(
+        private static async Task<BlobResultSegment> RetrieveBlobList(
             CloudBlobClient client, 
             string containerName, 
             string directory, 
             ILogger log)
         {
-            var fileList = await client
+            log.LogInformation("Retrieving blob list.");
+
+            return await client
                 .GetContainerReference(containerName)
                 .GetDirectoryReference(directory)
                 .ListBlobsSegmentedAsync(null);
+        }
 
-            foreach (var listBlobItem in fileList.Results)
+        private static void FtpBlobStreams(BlobResultSegment blobs, ILogger log)
+        {
+            if (!blobs.Results.Any())
             {
-                var blob = (CloudBlockBlob) listBlobItem;
-                log.LogInformation($"File found: {blob.Name}");
+                log.LogInformation("No blobs to ftp.");
+                return;
             }
 
-            return fileList;
+            log.LogInformation("Found blobs to ftp.");
+            foreach (var blob in blobs.Results)
+            {
+                var blobFile = (CloudBlockBlob) blob;
+                log.LogInformation($"Ftp file {blobFile.Name}");
+
+                Chilkat.Global obj = new Global();
+                obj.UnlockBundle("KARMAK.CBX032021_TDNDgEuT804Y");
+                SFtp sftp = new SFtp();
+                bool success = sftp.Connect("karmakqasftp.westus.cloudapp.azure.com", 22);
+                if (success)
+                {
+                    success = sftp.AuthenticatePw("karmaksftp", "K@rmakQa2019");
+                }
+
+                if (success)
+                {
+                    success = sftp.InitializeSftp();
+                }
+
+                if (!success)
+                {
+                    Console.WriteLine(sftp.LastErrorText);
+                    return;
+                }
+
+                var blobStream = GetBlob(blobFile);
+                StringBuilder sb = new StringBuilder();
+
+                var handle = sftp.OpenFile("/upload/testFile.xml", "readWrite", "createNew");
+                sftp.WriteFileBytes(handle, blobStream.Result.ToArray());
+                sftp.CloseHandle(handle);
+                
+                //client.UploadData(
+                //    "ftp://karmakqasftp.westus.cloudapp.azure.com/upload/testFile.xml", 
+                //    WebRequestMethods.Ftp.UploadFile, 
+                //    blobStream.Result.ToArray());
+            }
+        }
+
+        private static async Task<MemoryStream> GetBlob(
+            CloudBlockBlob file)
+        {
+            var ms = new MemoryStream();
+
+            await file.DownloadToStreamAsync(ms);
+
+            return ms;
         }
     }
 }
